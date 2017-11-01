7a0a4ec8dc547972851effa0e0eecd7fca842677
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2017-11-01 15:38:32 -0400

    Tell diesel that ingredients belong to recipes

--- src/models.rs
+++ src/models.rs
@@ -1,10 +1,13 @@
-#[derive(Queryable)]
+use schema::{recipes, ingredients};
+
+#[derive(Queryable, Identifiable)]
 pub struct Recipe {
     pub id: i32,
     pub name: String,
 }
 
-#[derive(Queryable)]
+#[derive(Queryable, Associations, Identifiable)]
+#[belongs_to(Recipe)]
 pub struct Ingredient {
     pub id: i32,
     pub amount: String,
